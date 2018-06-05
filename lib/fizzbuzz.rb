14fbe5bc30210e810ea3946ba7c5c11e4bcdc019
Layth <laythq@gmail.com>
2018-06-05 23:23:05 +0100

    TDD Pairing

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,12 +1,16 @@
 def fizzbuzz(number)
 
-	if number == 3
+	if number % 3 == 0 && number % 5 == 0
+		'fizzbuzz'
+
+	elsif number % 3 == 0
 		'fizz'
+	
+	elsif number % 5 == 0
+		'buzz'
+
+	else return number
+
 	end
-
-
-
-
-
 
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,6 +8,22 @@ describe 'fizbuzz' do
 		expect(fizzbuzz(3)).to eq 'fizz'
 	end
 
+	it 'returns "fizzbuzz" when passed 15' do
+		expect(fizzbuzz(6)).to eq 'fizz'
+	end
+
+	it 'returns "buzz" when passed 5' do
+		expect(fizzbuzz(5)).to eq 'buzz'
+	end
+
+	it 'returns "fizzbuzz" when passed 15' do
+		expect(fizzbuzz(15)).to eq 'fizzbuzz'
+	end
+
+	it 'returns number when passed 19' do
+		expect(fizzbuzz(19)).to eq 19
+	end
+
 
 
 end
